310babdab53017544ccae0014b588e2ea441290b
Ionut <ionut.pirvulescu@evolution-team.ro>
2018-02-16 12:28:09 +0200

    v1.4.1

--- config/laravel-enso.php
+++ config/laravel-enso.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version'             => '1.4.0',
+    'version'             => '1.4.1',
     'defaultRole'         => 'admin',
     'formattedTimestamps' => 'd-m-Y',
     'paths'               => [
